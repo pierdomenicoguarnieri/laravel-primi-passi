105981b2181dcf4a6c64c892fc6290377b6388b2
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-06-03 19:16:23 +0200

    add: markup and h1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Primi Passi</title>
-</head>
-<body>
-
-</body>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css' integrity='sha512-t4GWSVZO1eC8BM339Xd7Uphw5s17a86tIZIj8qRxhnKub6WoyhnrxeCIMeAqBPgdZGlCcG2PrZjMc+Wr78+5Xg==' crossorigin='anonymous'/>
+  <title>Laravel Primi Passi</title>
+  </head>
+  <body>
+    <div class="pg-main-wrapper bg-dark vh-100 text-white text-center py-5">
+      <h1>Hello World!</h1>
+    </div>
+  </body>
 </html>
